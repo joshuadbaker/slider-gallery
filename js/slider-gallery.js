ea26429321e32083d99c69565a96dbd52562396e
joshuadbaker <jbakercoder@gmail.com>
2015-01-07 19:56:41 -0500

    finished buttons

---
+++ js/slider-gallery.js
@@ -0,0 +1,32 @@
+$(function(){
+
+	var slideWidth = $('.slide').width(),
+		slideCount = $('.slide').size(),
+		pos = 0;
+
+	// set the width of the slide tray
+	$('#slide_tray').width(slideWidth * slideCount);
+
+	$('#next').click(function(){
+		if (pos < slideCount - 1) {
+			pos += 1;
+		} else {
+			pos = 0; 
+		}			
+		var slideAmount = slideWidth * pos;
+		$('#slide_tray').animate({right: slideAmount}, 600);
+	});
+
+	$('#prev').click(function(){
+		if (pos > 0) {
+			pos -= 1;
+		} else {
+			pos = slideCount - 1;
+		}
+		
+		var slideAmount = slideWidth * pos;
+		$('#slide_tray').animate({right: slideAmount}, 600);
+	});
+	
+});
+
